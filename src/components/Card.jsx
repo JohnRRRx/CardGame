fe472dea867657894093c8c6f0017e834fe2c90b
“JohnRRRx” <tom043838@gmail.com>
2025-08-04 14:10:45 +0900

    change: pictures and deploy config

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -11,8 +11,8 @@ const Card = ({ card, handleClick, flipped, disabled})=>{
     className={`card ${flipped ? "flipped" : ""}`}
     onClick={onClick}
   >
-    <img src="kao.png" alt="card back" width="200" className="rear"/>
-    <img src={card.src} alt="card pic" width="200" className="front"/>
+    <img src="/CardGame/kao.png" alt="card back" width="200" className="rear"/>
+    <img src={`/CardGame/${card.src}`} alt="card pic" width="200" className="front"/>
   </div>
 )}
 
